7be1731235d511118479a505460b1fece441cada
70474401 <estrella_yani_13@hotmail.com>
2021-06-15 01:38:45 -0500

    .

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Mi_proyecto.Data;
+
+[assembly: HostingStartup(typeof(Mi_proyecto.Areas.Identity.IdentityHostingStartup))]
+namespace Mi_proyecto.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
